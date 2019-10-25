466e3d953a9f9a43142dadb8012bc80940fe289f
jovandosen <jovandosen1994@gmail.com>
2019-10-25 14:06:48 +0200

    Admin scripts added

--- wp-content/plugins/forwardslash-real-estate-app/include/ForwardslashRealEstateAppAssets.php
+++ wp-content/plugins/forwardslash-real-estate-app/include/ForwardslashRealEstateAppAssets.php
@@ -5,6 +5,7 @@ class ForwardslashRealEstateAppAssets
 	public function __construct()
 	{
 		add_action('wp_enqueue_scripts', array($this, 'loadAssets'));
+		add_action('admin_enqueue_scripts', array($this, 'loadAdminAssets'));
 	}
 
 	public function loadAssets()
@@ -14,6 +15,12 @@ class ForwardslashRealEstateAppAssets
 
 		wp_register_style('css', plugins_url('/forwardslash-real-estate-app/assets/css/app.css'));
 		wp_enqueue_style('css');
+	}
+
+	public function loadAdminAssets()
+	{
+		wp_register_script('adminjs', plugins_url('/forwardslash-real-estate-app/assets/js/real-estate-admin.js'), array('jquery'),'1.1', true);
+		wp_enqueue_script('adminjs');
 	}
 }
 
